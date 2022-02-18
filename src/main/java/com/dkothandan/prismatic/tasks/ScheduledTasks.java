cdea504362d1c18a84322257bbcedf93545f1e38
diva <k.divakar@gmail.com>
2022-02-17 22:59:26 -0800

    Added azure config

--- src/main/java/com/dkothandan/prismatic/tasks/ScheduledTasks.java
+++ src/main/java/com/dkothandan/prismatic/tasks/ScheduledTasks.java
@@ -1,19 +1,13 @@
 package com.dkothandan.prismatic.tasks;
 
-import com.dkothandan.prismatic.utils.PrismaticConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Component
 public class ScheduledTasks {
-
-    private PrismaticConfig prismaticConfig;
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
@@ -21,10 +15,8 @@ public class ScheduledTasks {
 
     private final CampGroundReservationAvailabilityTask campGroundReservationAvailabilityTask;
 
-    public ScheduledTasks(CampGroundReservationAvailabilityTask campGroundReservationAvailabilityTask, PrismaticConfig prismaticConfig) {
+    public ScheduledTasks(CampGroundReservationAvailabilityTask campGroundReservationAvailabilityTask) {
         this.campGroundReservationAvailabilityTask = campGroundReservationAvailabilityTask;
-        this.prismaticConfig = prismaticConfig;
-        System.out.println(prismaticConfig.getSendGridAPIKey());
     }
 
 //    @Scheduled(fixedRate = (1000 * 60 * 30)) // 30 mins
